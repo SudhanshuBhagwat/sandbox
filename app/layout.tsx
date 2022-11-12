c5ae0087b6416c384535ebd0fa5d600976093779
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-11-12 19:27:52 +0530

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
-import './globals.css'
+import 'tailwindcss/tailwind.css'
+import { Inter } from "@next/font/google"
+
+const inter = Inter({
+  subsets: ['latin']
+});
 
 export default function RootLayout({
   children,
@@ -6,11 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={inter.className}>
       <head />
       <body>{children}</body>
     </html>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,js,jsx,css,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
